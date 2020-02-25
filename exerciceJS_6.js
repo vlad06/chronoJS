c0a531c9424c5476e6fb258f96e05f57c107e1c0
vlad06 <vladimirhuppert@hotmail.fr>
2020-02-25 21:22:07 +0100

    ajout des millisecondes au compteur

--- exerciceJS_6.js
+++ exerciceJS_6.js
@@ -5,20 +5,19 @@ var stop = document.getElementById("stop");		// référence bouton stop
 var compteur = document.getElementById("compteur"); //référence span compteur
 var tpsEcoule=0;
 
-(function() {
-  start.paramTps = tpsEcoule;
-  start.addEventListener('click', startChrono);
-  pause.addEventListener('click', togglePauseChrono);
-  stop.addEventListener('click', stopChrono); 
+(function() {	//fonction autoexécutable, se charge dès que le document html à été chargé
+  start.paramTps = tpsEcoule; //on rajoute un attribut à l'objet 
+  start.addEventListener('click', startChrono);	//on écoute les actions click sur le bouton start
+  pause.addEventListener('click', pauseChrono);	//on écoute les actions click sur le bouton pause
+  stop.addEventListener('click', stopChrono); 	//on écoute les actions click sur le bouton stop
 }());
 
 function startChrono(e) {
 	console.log("fonction start");
-	if(pause.innerHTML == "Reprendre") {
-		pause.innerHTML = "Pause";
-		pause.removeEventListener('click', startChrono);
-		pause.addEventListener('click', togglePauseChrono);
-	}
+	pause.innerHTML = "Pause";	//le bouton html prend la valeur pause, car à ce moment là chrono tourne
+	pause.removeEventListener('click', startChrono); //on supprime le lien entre le bouton pause et la fonction start,
+													//celle-ci ne sert que lors du clic sur le bouton "Reprendre" du chrono
+	pause.addEventListener('click', pauseChrono);	
 	start.removeEventListener('click', startChrono);
 	start.style.visibility = "hidden";
 	pause.style.visibility = "visible";
@@ -26,46 +25,79 @@ function startChrono(e) {
 	if(stop.addEventListener) {
 		stop.addEventListener('click', stopChrono);
 	}
-  var startTime = new Date();
-		decompte = setInterval(
+  	// var startTime = new Date();
+	// decompte = setInterval(
+	// 	function() {
+	// 		var seconds = Math.round((new Date().getTime()-startTime.getTime()) / 1000 + e.target.paramTps);
+	// 		var hours = parseInt(seconds / 3600);
+	// 		var minutes = parseInt(seconds / 60);
+	// 		seconds = seconds % 60;
+	// 		compteur.innerHTML = addZero(hours) + ":" + addZero(minutes) + ":" + addZero(seconds);
+	// 		tpsEcoule++;
+	// 	}, 1000
+	// );
+	var startTime = new Date();
+	decompte = setInterval(
 		function() {
-			var seconds = Math.round((new Date().getTime()-startTime.getTime()) / 1000 + e.target.paramTps);
+			// console.log(new Date().getTime()-startTime.getTime());
+			var milliSeconds = new Date().getTime()-startTime.getTime() + e.target.paramTps;
+			console.log(milliSeconds % 1000);
+			var seconds = parseInt(milliSeconds / 1000);
 			var hours = parseInt(seconds / 3600);
 			var minutes = parseInt(seconds / 60);
-			seconds = seconds % 60;
-			compteur.innerHTML = addZero(hours) + ":" + addZero(minutes) + ":" + addZero(seconds);
-			tpsEcoule++;
-		}, 1000
+			milliSeconds %= 1000;
+			seconds %= 60;
+			// console.log(seconds);
+			compteur.innerHTML = addZero(hours) + ":" + addZero(minutes) + ":" + addZero(seconds) + ":" + addZeros(milliSeconds);
+			// tpsEcoule++;
+		}, 1
 	);
 }
 
-function togglePauseChrono() {
+function pauseChrono() {
 	console.log("fonction pause");
-	if(pause.innerHTML == "Pause") {
-		pause.innerHTML = "Reprendre";
-		clearInterval(decompte);
-		pause.paramTps = tpsEcoule;
-		pause.removeEventListener('click', togglePauseChrono);
-		pause.addEventListener('click', startChrono);
-	}
+	pause.innerHTML = "Reprendre";
+	clearInterval(decompte);
+	var tempsQuiPasse = compteur.innerHTML;
+	console.log(tempsQuiPasse);
+	// console.log(convertStringToTime(tempsQuiPasse));
+	pause.paramTps = convertStringToTime(tempsQuiPasse);
+	// pause.paramTps = tpsEcoule;
+	// console.log(pause.paramTps);
+	pause.removeEventListener('click', pauseChrono);
+	pause.addEventListener('click', startChrono);
 }
 
 function stopChrono() {
 	console.log("fonction stop");
 	clearInterval(decompte);
-	pause.removeEventListener('click', startChrono);
-	pause.removeEventListener('click', togglePauseChrono);
+	(pause.innerHTML=="Pause") ? pause.removeEventListener('click', startChrono) : pause.removeEventListener('click', pauseChrono);
 	stop.removeEventListener('click', stopChrono);
 	start.addEventListener('click', startChrono);
-	pause.addEventListener('click', togglePauseChrono);
-	
+	pause.addEventListener('click', pauseChrono);
+	tpsEcoule = 0;
+	start.paramTps = tpsEcoule;
+	compteur.innerHTML = "00:00:00:000";
+ 	start.style.visibility = "visible";
+ 	pause.style.visibility = "hidden";
+	stop.style.visibility = "hidden";
 	pause.innerHTML = "Pause";
-	compteur.innerHTML = "00:00:00";
-  start.style.visibility = "visible";
-  pause.style.visibility = "hidden";
-  stop.style.visibility = "hidden";	
 }
 
 function addZero(nb) {
-    return nb < 10 ? "0"+nb : nb;
+	//si c'est un chiffre, on rajoute un 0 devant, si c'est un nombre on ne fait rien
+    return nb < 10 ? "0" + nb : nb;
+}
+function addZeros(nb) {
+	//si c'est un chiffre, on rajoute deux 0 devant, ci c'est un nombre à "2 chiffre" on rajoute un 0, sinon on ne fait rien
+	return (nb < 10 ? "00"+nb : (nb < 100 ? "0" + nb : nb));
+}
+function convertStringToTime(stringTemps) {
+	var timePass=0;
+	var strToInt;
+	//la string récupérée à ce format ->  00:00:00:000, on ne garde que les chiffres
+	strToInt = stringTemps.slice(0,2)+stringTemps.slice(3,5)+stringTemps.slice(6,8)+stringTemps.slice(9,12);
+	//une fois qu'il ne reste que les chiffres on convertit la string en int
+	strToInt = parseInt(strToInt);
+	return strToInt;
 }
