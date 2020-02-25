b483c6a28e89607514d827c039e14264f7fa611f
vlad06 <vladimirhuppert@hotmail.fr>
2020-02-25 16:00:42 +0100

    the chrono js is working

--- exerciceJS_6.js
+++ exerciceJS_6.js
@@ -1,67 +1,71 @@
-var start = document.getElementById("start");
-var pause = document.getElementById("pause");
-var stop = document.getElementById("stop");
-var compteur = document.getElementById("compteur");
-var timePass=0;
+//référence des 3 objets correspondant aux 3 boutons et au span
+var start = document.getElementById("start");	// référence bouton start
+var pause = document.getElementById("pause"); // référence bouton pause
+var stop = document.getElementById("stop");		// référence bouton stop
+var compteur = document.getElementById("compteur"); //référence span compteur
+var tpsEcoule=0;
+
 (function() {
-    start.paramTps = timePass;
-    pause.paramTps = timePass;
-    start.addEventListener('click', startChrono);
-    pause.addEventListener('click', togglePauseChrono);
-    stop.addEventListener('click', stopChrono);   
+  start.paramTps = tpsEcoule;
+  start.addEventListener('click', startChrono);
+  pause.addEventListener('click', togglePauseChrono);
+  stop.addEventListener('click', stopChrono); 
 }());
 
-
-var timer;
-
-
-function startChrono(event) {
-    start.removeEventListener('click', startChrono);
-    start.style.visibility = "hidden";
-    pause.style.visibility = "visible";
-    stop.style.visibility = "visible";
-    var startTime = new Date();
-    decompte = setInterval(function() {
-    var seconds = Math.round((new Date().getTime()-startTime.getTime()) / 1000 + event.target.paramTps);
-    var hours = parseInt(seconds / 3600);
-    var minutes = parseInt(seconds / 60);
-    seconds = seconds % 60;
-    compteur.innerHTML = addZero(hours) + ":" + addZero(minutes) + ":" + addZero(seconds);
-    timePass++;
-}, 1000);
+function startChrono(e) {
+	console.log("fonction start");
+	if(pause.innerHTML == "Reprendre") {
+		pause.innerHTML = "Pause";
+		pause.removeEventListener('click', startChrono);
+		pause.addEventListener('click', togglePauseChrono);
+	}
+	start.removeEventListener('click', startChrono);
+	start.style.visibility = "hidden";
+	pause.style.visibility = "visible";
+	stop.style.visibility = "visible";
+	if(stop.addEventListener) {
+		stop.addEventListener('click', stopChrono);
+	}
+  var startTime = new Date();
+		decompte = setInterval(
+		function() {
+			var seconds = Math.round((new Date().getTime()-startTime.getTime()) / 1000 + e.target.paramTps);
+			var hours = parseInt(seconds / 3600);
+			var minutes = parseInt(seconds / 60);
+			seconds = seconds % 60;
+			compteur.innerHTML = addZero(hours) + ":" + addZero(minutes) + ":" + addZero(seconds);
+			tpsEcoule++;
+		}, 1000
+	);
 }
 
 function togglePauseChrono() {
-    if(pause.innerHTML == "Pause") {
-       pause.value = "Reprendre";
-    }
-    if(pause.innerHTML == "Reprendre") {
-        pause.innerHTML = "Pause";
-        clearInterval(decompte);
-    }
-    console.log(pause.innerHTML=="Pause");
-    console.log(pause.innerHTML);
+	console.log("fonction pause");
+	if(pause.innerHTML == "Pause") {
+		pause.innerHTML = "Reprendre";
+		clearInterval(decompte);
+		pause.paramTps = tpsEcoule;
+		pause.removeEventListener('click', togglePauseChrono);
+		pause.addEventListener('click', startChrono);
+	}
 }
 
 function stopChrono() {
-
+	console.log("fonction stop");
+	clearInterval(decompte);
+	pause.removeEventListener('click', startChrono);
+	pause.removeEventListener('click', togglePauseChrono);
+	stop.removeEventListener('click', stopChrono);
+	start.addEventListener('click', startChrono);
+	pause.addEventListener('click', togglePauseChrono);
+	
+	pause.innerHTML = "Pause";
+	compteur.innerHTML = "00:00:00";
+  start.style.visibility = "visible";
+  pause.style.visibility = "hidden";
+  stop.style.visibility = "hidden";	
 }
 
 function addZero(nb) {
     return nb < 10 ? "0"+nb : nb;
 }
-
-// var btn = document.querySelector('input');
-// var txt = document.querySelector('p');
-
-// btn.addEventListener('click', updateBtn);
-
-// function updateBtn() {
-//     if (btn.value === 'Démarrer la machine') {
-//       btn.value = 'Arrêter la machine';
-//       txt.textContent = 'La machine est démarrée !';
-//     } else {
-//       btn.value = 'Démarrer la machine';
-//       txt.textContent = 'La machine est arrêtée.';
-//     }
-//   }
